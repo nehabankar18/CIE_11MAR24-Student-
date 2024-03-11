427115d1fde814f250756d51474f2fb34049da7a
Neha <01fe22bca040@gmail.com>
2024-03-11 03:28:46 -0700

    program of student name and age

---
+++ student.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+
+public class student{
+    static String name="Neha";
+    Date Dob=new Date();
+    static int age=20;
+public static void display(){
+System.out.println(name);
+System.out.println(age);
+} 
+}
